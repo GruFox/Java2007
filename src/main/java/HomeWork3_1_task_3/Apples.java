bf8316497558feb2e63571a34b365bed9ec9c2ca
Gru <energimen@yandex.ru>
2022-05-03 07:04:45 +0300

    HomeWork3_1

---
+++ src/main/java/HomeWork3_1_task_3/Apples.java
@@ -0,0 +1,7 @@
+package HomeWork3_1_task_3;
+
+public class Apples extends Fruits{
+    public Apples() {
+        super("Apple", 1.0f);
+    }
+}
---
+++ src/main/java/HomeWork3_1_task_3/Oranges.java
@@ -0,0 +1,7 @@
+package HomeWork3_1_task_3;
+
+public class Oranges extends Fruits{
+    public Oranges() {
+        super("Orange", 1.5f);
+    }
+}
